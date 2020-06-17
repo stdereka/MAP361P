59e9e3ee69cd8fe8d29398fa862a7f7cbb61c9d6
stdereka <st.dereka@gmail.com>
2020-06-18 00:01:19 +0300

    Hot fix!

--- spellbook.py
+++ spellbook.py
@@ -2,9 +2,6 @@ from scipy import stats
 import numpy as np
 
 
-EPS = 1e-2
-
-
 class Distribution:
     def __init__(self, pdf: callable, cdf: callable, ppf: callable):
         self.pdf = pdf
@@ -27,24 +24,24 @@ normal_standard = Distribution(stats.norm.pdf, stats.norm.cdf, stats.norm.ppf)
 
 
 def g_tilde_pdf(x: np.ndarray):
-    return np.abs(x)*np.exp(-x**2)
+    return np.abs(x)*np.exp(-x**2/2)/2
 
 
 def g_tilde_cdf(x: np.ndarray):
     res = np.zeros_like(x, np.float64)
     int_1 = x <= 0
-    res[int_1] = np.exp(-x[int_1]**2)/2
+    res[int_1] = np.exp(-x[int_1]**2/2)/2
     int_2 = x > 0
-    res[int_2] = 1 - np.exp(-x[int_2]**2)/2
+    res[int_2] = 1 - np.exp(-x[int_2]**2/2)/2
     return res
 
 
 def g_tilde_ppf(x: np.ndarray):
     res = np.zeros_like(x, np.float64)
     int_1 = np.logical_and(x >= 0, x <= 1/2)
-    res[int_1] = -(-np.log(2*x[int_1]))**0.5
+    res[int_1] = -(-2*np.log(2*x[int_1]))**0.5
     int_2 = np.logical_and(x >= 1/2, x <= 1)
-    res[int_2] = (-np.log(2-2*x[int_2]))**0.5
+    res[int_2] = (-2*np.log(2-2*x[int_2]))**0.5
     return res
 
 
@@ -66,7 +63,7 @@ def sample_from_distribution(dst: Distribution, n_samples: int):
 
 def run_simulation(h: callable, g: Distribution, pi: Distribution, n_samples: int):
     selection = sample_from_distribution(g, n_samples)
-    estimated = h(selection)*(pi.pdf(selection)+EPS)/(g.pdf(selection)+EPS)
+    estimated = h(selection)*pi.pdf(selection)/g.pdf(selection)
 
     estimated_values = [estimated[:n].mean() for n in range(1, n_samples)]
 
@@ -95,6 +92,19 @@ class AdaptiveSampling:
             res += self.alphas[n] * dist.pdf(x)
         return res
 
+    def sample(self, n_samples=10000):
+        labels = sample_labels(self.alphas, n_samples)
+        selection = np.zeros(n_samples, np.float64)
+        for label, count in zip(*np.unique(labels, return_counts=True)):
+            if count != 0:
+                selection[labels == label] = sample_from_distribution(self.dists[label], count)
+        return selection, labels
+
+    def estimate(self, n_samples=10000):
+        selection, _ = self.sample(n_samples)
+        estimated = self.h(selection) * self.pi.pdf(selection) / self._compose_dists(selection)
+        return estimated.mean()
+
     def fit(self, n_samples=10000, max_iter=1000, tolerance=1e-6):
         alphas_log = []
         approx_log = []
@@ -102,24 +112,21 @@ class AdaptiveSampling:
 
         for i in range(max_iter):
             alphas_log.append(self.alphas)
-            labels = sample_labels(self.alphas, n_samples)
-            selection = np.zeros(n_samples, np.float64)
-            for label, count in zip(*np.unique(labels, return_counts=True)):
-                if count != 0:
-                    selection[labels == label] = sample_from_distribution(self.dists[label], count)
 
-            estimated = self.h(selection)*(self.pi.pdf(selection)+EPS)/(self._compose_dists(selection)+EPS)
+            selection, labels = self.sample(n_samples)
+
+            estimated = self.h(selection)*self.pi.pdf(selection)/self._compose_dists(selection)
             approx = np.mean(estimated)
             approx_log.append(approx)
             variance_log.append(estimated.var())
 
-            denominator = np.sum((self.h(selection)*(self.pi.pdf(selection)+EPS)/(self._compose_dists(selection)+EPS))**2)
+            denominator = np.sum((self.h(selection)*self.pi.pdf(selection)/self._compose_dists(selection))**2)
 
             new_alphas = np.zeros_like(self.alphas, np.float64)
 
             for label in np.unique(labels):
                 subsel = selection[labels == label]
-                nominator = np.sum((self.h(subsel)*(self.pi.pdf(subsel)+EPS)/(self._compose_dists(subsel)+EPS))**2)
+                nominator = np.sum((self.h(subsel)*self.pi.pdf(subsel)/self._compose_dists(subsel))**2)
                 new_alphas[label] = nominator/denominator
 
             delta = np.abs(new_alphas - self.alphas).mean()
